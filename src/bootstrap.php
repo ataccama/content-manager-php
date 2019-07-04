3b99b529b6ff178ba75f10e7f108e16ff4ae1e9c
Miroslav Štolfa <miroslav.stolfa@ataccama.com>
2019-07-04 14:05:15 +0200

    autoload

---
+++ src/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+    require __DIR__ . "/Exceptions/ContentNotFound.php";
+    require __DIR__ . "/Exceptions/DuplicityException.php";
+    require __DIR__ . "/Exceptions/NotInitialized.php";
+
+    require __DIR__ . "/Utils/Content.php";
+    require __DIR__ . "/Utils/ContentFilter.php";
+    require __DIR__ . "/Utils/ContentPart.php";
+    require __DIR__ . "/Utils/Language.php";
+
+    require __DIR__ . "/Inputs/ContentLoader.php";
+    require __DIR__ . "/Inputs/IStorage.php";
